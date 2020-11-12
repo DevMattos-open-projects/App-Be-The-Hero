e672b1d679f72ee7e67b86f4d1c651e5275bbf07
Rafael Matos <rafaelmatostj@gmail.com>
2020-11-11 23:39:47 -0300

    init

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+    return (
+     <NavigationContainer>
+         <AppStack.Navigator>
+            <AppStack.Screen/>
+         </AppStack.Navigator>
+     </NavigationContainer>
+    );
+}
